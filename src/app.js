3361e4471000c3dcf698a671274c9bc4fc4d4f72
Vikrant <vikrant.immeasurable@gmail.com>
2024-12-18 00:50:07 +0530

    exploring routing in express

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-console.log("learning backend!")
-
 const express = require("express")
 
 const app = express()
 
-app.use("/", (req, res) => {
-    res.end("Hello from the ser11ver!")
+
+// this will only handle GET call to /user
+app.get("/user", (req, res) => {
+    console.log("req", req.url)
+    res.send({firstName: "Vikrant", lastName: "Singh"})
 })
 
-app.use("/hello", (req, res) => {
-    res.end("Hello from the gello!")
+app.post("/user", (req, res) => {
+    console.log("Save data to the database")
+    res.send("Data successfully saved to the database")
+})
+
+// this "use" will match all the HTTP method API calls to "/test"
+app.use("/test", (req, res) => {
+    res.send("Hello from the server: /test!")
 })
 
 app.listen(3000)
